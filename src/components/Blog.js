af66043efc96f824efb8039eaaf6268abec12157
Daniel Chen <dchen0058@gmail.com>
2022-01-10 14:15:11 +1100

    Completed 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const buttonLabel = viewDetails ? 'hide' : 'view'
+
+  const handleDetailClick = (event) =>{
+    setViewDetails(!viewDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={handleDetailClick}>{buttonLabel}</button>
+      </div> 
+
+      {viewDetails ?
+      <>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>
+          {blog.author}
+        </div>
+      </> : 
+      <></>}
+      
+    </div>
+  )
+}
 
 export default Blog
