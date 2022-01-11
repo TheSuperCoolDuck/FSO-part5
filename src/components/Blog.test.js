c67fd4f50af968474dfe3a83afaebcc7de2720ef
Daniel Chen <dchen0058@gmail.com>
2022-01-11 18:04:18 +1100

    completed 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -55,3 +55,27 @@ test('displays url and likes when button is pressed',() => {
     `likes ${blog.likes}`
   )
 })
+
+test('clicking the like button twice will call like func twice', ()=>{
+  const blog = {
+    title: 'Component testing',
+    author: 'John Doe',
+    url: 'localhost/',
+    likes: 0
+  }
+
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <Blog blog={blog} likeBlog={mockHandler} deleteBlog={() => null}/>
+  )
+
+  const viewButton = component.getByText('view')
+  fireEvent.click(viewButton)
+
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
