2beba4666f575daccdd185dfbb9155214071c15d
Daniel Chen <dchen0058@gmail.com>
2022-01-11 17:55:39 +1100

    completed 5.13

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import {render} from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders correct content', ()=>{
+test('renders correct content', () => {
   const blog = {
     title: 'Component testing',
     author: 'John Doe',
@@ -12,7 +12,7 @@ test('renders correct content', ()=>{
   }
 
   const component = render(
-    <Blog blog={blog} likeBlog={()=>null} deleteBlog={()=>null}/>
+    <Blog blog={blog} likeBlog={() => null} deleteBlog={() => null}/>
   )
 
   expect(component.container).toHaveTextContent(
@@ -28,6 +28,30 @@ test('renders correct content', ()=>{
   )
 
   expect(component.container).not.toHaveTextContent(
+    `likes ${blog.likes}`
+  )
+})
+
+test('displays url and likes when button is pressed',() => {
+  const blog = {
+    title: 'Component testing',
+    author: 'John Doe',
+    url: 'localhost/',
+    likes: 0
+  }
+
+  const component = render(
+    <Blog blog={blog} likeBlog={() => null} deleteBlog={() => null}/>
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    blog.url
+  )
+
+  expect(component.container).toHaveTextContent(
     `likes ${blog.likes}`
   )
 })
